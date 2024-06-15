71c090b0066368e3aaa9359c5841d73e2c28f4bb
Vitor Bezerra <vitorhenrique908@gmail.com>
2024-06-15 15:30:08 -0300

    Adiciona ações para o player: andar e pular

--- scripts/playernisia.gd
+++ scripts/playernisia.gd
@@ -7,6 +7,8 @@ const JUMP_FORCE = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animation := $anim as AnimatedSprite2D
+var is_jumping := false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,13 +18,22 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction * 0.6
+		if !is_jumping:
+			animation.play("run")
+	elif is_jumping:
+		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
