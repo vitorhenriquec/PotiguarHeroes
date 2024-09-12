4d6e24eca7a5e7bfaf28b0e99165aef87fb22167
Vitor Henrique Coelho Bezerra <vitorhenrique908@gmail.com>
2024-09-11 21:05:15 -0300

    Faz alterações no level design da fase

--- levels/worldisabel.gd
+++ levels/worldisabel.gd
@@ -7,15 +7,15 @@ var actual_score = 0
 var score_messages = {
 	0:  "",
 	1:  "Fundou uma escola na Ribeira, em Natal",
-	2:  "Foi educadora, historiadora e poetisa, destacando-se\n por seu papel pioneiro na educação",
+	2:  "Foi educadora, historiadora e poetisa,\n destacando-se por seu papel \npioneiro na educação",
 	3:  "Isabel começou a ensinar aos 27 anos",
-	4:  "Defendeu fervorosamente o ensino público para mulheres",
-	5:  "Promoveu uma revolução no magistério em Natal,\nelevando o status da profissão docente.",
-	6:  "Também se expressou através da poesia,\n deixando um legado literário",
-	7:  "Documentou e valorizou a cultura local e as memórias de sua época",
-	8:  "Ensinava com atividades lúdicas e estímulava a criatividade",
-	9:  "Lutou para que as mulheres tivessem as mesmas oportunidades educacionais que os homens",
-	10:  "se envolvia ativamente na gestão escolar, garantindo que sua escola\n fosse um ambiente acolhedor e produtivo"
+	4:  "Defendeu fervorosamente \no ensino público para mulheres",
+	5:  "Promoveu uma revolução no \nmagistério em Natal,\nelevando o status da \nprofissão docente.",
+	6:  "Também se expressou \natravés da poesia,\n deixando um legado literário",
+	7:  "Documentou e valorizou \na cultura local e\nas memórias de sua época",
+	8:  "Ensinava com atividades \nlúdicas e estímulava\na criatividade",
+	9:  "Lutou para que as \nmulheres tivessem as\nmesmas oportunidades\n educacionais que os homens",
+	10:  "Se envolvia ativamente\nna gestão escolar,\ngarantindo que sua\n escola fosse um\n ambiente acolhedor e produtivo"
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -31,7 +31,7 @@ func _process(delta):
 	$hud/control/container/score_container/score_counter.text = PlayerState.score_count()
 	
 	if PlayerState.get_score() != actual_score and $player != null:
-		$hud/control/message_container/score_message/scoremessage.text = score_messages[PlayerState.get_score()]
+		$hud/control/message_container/score_message.text = score_messages[PlayerState.get_score()]
 		actual_score = PlayerState.get_score()
 		await get_tree().create_timer(3).timeout
 		$hud/control/message_container/score_message.text = ""
